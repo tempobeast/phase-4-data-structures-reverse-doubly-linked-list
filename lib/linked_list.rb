1220423b0ca27dc288bb42c8e9b1119840f19b00
Zach <shaw.zacht@gmail.com>
2022-08-08 12:52:08 -0700

    commit tree 68d99cf06a9b072004729efe5b19367691f4e80c

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,28 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    return if head.nil?
+    curr = head
+    if head == tail
+      nil
+    else
+      ptr1 = head
+      ptr2 = head.next_node
+      ptr1.next_node = nil
+      ptr1.prev_node = ptr2
+      while ptr2
+        ptr2.prev_node = ptr2.next_node
+        ptr2.next_node = ptr1
+        ptr1 = ptr2
+        ptr2 = ptr2.prev_node
+      end
+      last = tail
+      self.tail = head
+      self.head = last
+      self
+    end
   end
+
+
+
 end
